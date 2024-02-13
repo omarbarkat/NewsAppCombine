a9345b3611f6305a6b52c932df95d9905b19d373
Mr.IOS <omarbarkat7@gmail.com>
2024-02-13 13:44:55 +0200

    Initial Commit

---
+++ NewsAppCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsAppCombine
+//
+//  Created by Omar barkat on 13/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsAppCombine/NewsAppCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppCombineApp.swift
+//  NewsAppCombine
+//
+//  Created by Omar barkat on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
